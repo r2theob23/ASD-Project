cc35df10d70b1fe2dd6dbe524a8db63641b5f3ff
Robert Smith <r2theob23@gmail.com>
2013-03-20 15:52:10 -0400

    modified index, main.js and map.js files in order to get ajax working…but it still isn't

--- WK3/asdcouch/app/_attachments/js/main.js
+++ WK3/asdcouch/app/_attachments/js/main.js
@@ -1,4 +1,21 @@
-$(document).ready(function() {
+$('#testData').on('pageinit', function() {
 	$.ajax({
-		"url": 
+		"url": "_view/class",
+		"dataType": "json",
+		"success": function(data) {
+			$.each(data.rows, function(index, class){
+				var class = class.value.classTitle;
+				var assignment = class.value.assignmentName;
+				var email = class.value.teacherEmail;
+				var dueDate = class.value.duedate;
+				var note = class.value.notes;
+				$("#classlist").append(
+					$('<li>').append(
+						$('<a>').attr('href', '#')
+							.text(class)
+					)
+				);
+			});
+		} 
+	});	
 });
--- WK3/asdcouch/app/views/class/map.js
+++ WK3/asdcouch/app/views/class/map.js
@@ -1,8 +1,11 @@
 function (doc) {
 	if (doc._id.substr(0, 6) ==="class:") {
-		emit(doc._id, {
+		emit(doc._id.substr(6), {
 			"class": doc.classTitle[1],
-			"assignment": doc.assignmentName[1]
-		})
+			"assignment": doc.assignmentName[1],
+			"instructor email": doc.teacherEmail[1],
+			"due date": doc.dudate[1],
+			"notes": doc.notes[1]		
+			})
 	}
 };
