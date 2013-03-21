42068b4d10b75af6b2decf5be31eecbca0349698
Robert Smith <r2theob23@gmail.com>
2013-03-20 23:35:01 -0400

    got ajax function to pull data from couch into my app

--- WK3/asdcouch/app/_attachments/js/main.js
+++ WK3/asdcouch/app/_attachments/js/main.js
@@ -1,21 +1,20 @@
-$('#testData').on('pageinit', function() {
+$('#testData').on('pageinit', function(){
 	$.ajax({
 		"url": "_view/class",
 		"dataType": "json",
-		"success": function(data) {
-			$.each(data.rows, function(index, class){
-				var class = class.value.classTitle;
-				var assignment = class.value.assignmentName;
-				var email = class.value.teacherEmail;
-				var dueDate = class.value.duedate;
-				var note = class.value.notes;
-				$("#classlist").append(
-					$('<li>').append(
-						$('<a>').attr('href', '#')
-							.text(class)
-					)
-				);
+		"success": function(data){
+			$.each(data.rows, function(index, project){
+				var classTitle = project.value.class;
+				var assignmentName= project.value.assignment;
+				$('#classlist').append(
+						$('<li>').append(
+							$('<a>').attr('href', '#')
+								.text(classTitle)
+						)
+					);
 			});
-		} 
-	});	
-});
+			$('#classlist').listview('refresh');
+		}
+	})
+})
+	
